2a1fbecc39c8bed0a22b01f37e57184895ec31ba
Azzedine bk <106198891+irdiris@users.noreply.github.com>
2024-10-14 20:11:55 +0100

    Update Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,18 +1,1 @@
-import React from 'react';
 
-const Sidebar = ({ setActiveView }) => {
-  return (
-    <div className="sidebar">
-      <h2 className='logoDash'>ManagementDash</h2>
-      <button onClick={() => setActiveView('updateEmployee')}>Update Employee</button>
-      <button onClick={() => setActiveView('employees')}>Employees</button>
-      <button onClick={() => setActiveView('feedback')}>Check Feedback</button>
-      <button onClick={() => setActiveView('viewTasks')}>View Tasks</button> 
-      <button onClick={() => setActiveView('groups')}>Groups</button>
-      <button onClick={() => setActiveView('settings')}>Settings</button>
-      <button onClick={() => console.log('Logout clicked')}>Logout</button>
-    </div>
-  );
-};
-
-export default Sidebar;
