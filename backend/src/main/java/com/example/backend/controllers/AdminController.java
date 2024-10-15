18057104863532f4b7a20be81c9fa8599353685f
Irdiris <azedine.baka@unis-constantine2.dz>
2024-10-15 09:09:38 +0100

    fixed conflicts

--- backend/src/main/java/com/example/backend/controllers/AdminController.java
+++ backend/src/main/java/com/example/backend/controllers/AdminController.java
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.format.DecimalStyle;
 import java.util.List;
 
-@Controller
+@RestController
 public class AdminController {
     private final DepartmentServiceInterface departmentService;
     private final ManagerServiceInterface managerService;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/ManagerView';
@@ -39,3 +40,4 @@ function App() {
 }
 
 export default App;
+
